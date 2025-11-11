0053223116b9c86d60bb1493981773cd30baaadf
Daniel Morales <flacko.yo@gmail.com>
2025-11-10 22:09:01 -0300

    se modifica el pool

--- database/pool.js
+++ database/pool.js
@@ -1,11 +1,8 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // importante para Supabase
   allowExitOnIdle: true,
 });
 
