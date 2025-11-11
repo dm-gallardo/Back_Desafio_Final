6dac3e375a0f6979b63c9bb01690c725866cfcd7
Daniel Morales <flacko.yo@gmail.com>
2025-11-11 02:50:46 -0300

    Update pool

--- database/pool.js
+++ database/pool.js
@@ -4,7 +4,6 @@ import { Pool } from 'pg';
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    require: true,
     rejectUnauthorized: false,
   },
   allowExitOnIdle: true,
