43c34fca4b87e59ce3d804b5f7565f48da05f07e
Daniel Morales <flacko.yo@gmail.com>
2025-11-11 00:55:44 -0300

    Update Pool.js
    se modifica el pool a un pool que nos permita por vercel

--- database/pool.js
+++ database/pool.js
@@ -1,25 +1,13 @@
 import 'dotenv/config';
 import { Pool } from 'pg';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const dbConfig = {
-  host: isProduction
-    ? process.env.DB_HOST_PROD || 'db-pool.vuwgaqfrgvfsjsznmoir.supabase.co'
-    : process.env.DB_HOST || 'db.vuwgaqfrgvfsjsznmoir.supabase.co',
-  port: isProduction
-    ? process.env.DB_PORT_PROD || 6543
-    : process.env.DB_PORT || 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     require: true,
     rejectUnauthorized: false,
   },
   allowExitOnIdle: true,
-};
-
-const pool = new Pool(dbConfig);
+});
 
 export { pool };
