ebc65ae69616c065cb2016c37604c61559761fa2
Daniel Morales <136399979+dm-gallardo@users.noreply.github.com>
2025-11-10 22:10:38 -0300

    Refactor database connection settings

--- database/pool.js
+++ database/pool.js
@@ -1,8 +1,12 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // importante para Supabase
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
   allowExitOnIdle: true,
 });
 
